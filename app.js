8436ac3e720c2dcb085c925594a3d5c037310b9f
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-21 22:15:06 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // write code here
+
+let walletMoney;
+let fatherName;
+let motherName;
+let homeAddress;
+let universeAge;
+
+walletMoney = 20
+fatherName = "Nick"
+motherName = "Lyla"
+homeAddress = "114 Bangkok"
+universeAge = 9999999999
